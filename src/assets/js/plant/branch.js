ec75d1845be21dbbb81217742d0c922ab1fa7880
Louis Van Aken <vanakenlouis@gmail.com>
2023-10-24 18:14:17 +0200

    Added growing animation

--- src/assets/js/plant/branch.js
+++ src/assets/js/plant/branch.js
@@ -7,37 +7,52 @@ const COLOR_GREEN_100 = '0x53621E'
 const COLOR_GREEN_200 = '0x48551E'
 const COLOR_GREEN_300 = '0x374015'
 
+const SEGMENT_MIN_LENGTH = 20
+const SEGMENT_MAX_LENGTH = 40
+
 export default class Branch {
   constructor(builder, x, y) {
     this.builder = builder;
-    this.length = 30;
-
     this.points = [];
     this.addPoint(x, y)
 
-    this.lastPointX
+    this.growingSegmentEnd = this.addPoint(x, y)
+    this.growingSegmentLength = 0
+    this.growingSegmentMaxLength = SEGMENT_MAX_LENGTH
+  }
 
-
-    this.addPoint(x, y - this.length)
-    this.addPoint(x+2, y - this.length*2)
-    this.addPoint(x+2, y - this.length*3)
-    this.addPoint(x, y - this.length*4)
-    this.addPoint(x-2, y - this.length*5)
-    this.addPoint(x-3, y - this.length*6)
-    this.addPoint(x-1, y - this.length*7)
-    this.addPoint(x, y - this.length*8)
+  randomSegmentLength() {
+    return Math.floor(Math.random() * (SEGMENT_MAX_LENGTH - SEGMENT_MIN_LENGTH + 1) + SEGMENT_MIN_LENGTH)
   }
 
   addPoint(x, y) {
-    this.points.push(new PIXI.Point(x, y))
+    let newPoint = new PIXI.Point(x, y)
+    this.points.push(newPoint)
+    return newPoint
+  }
+
+  grow() {
+    // Grow current branch segment
+    if (this.growingSegmentLength < this.growingSegmentMaxLength) {
+      this.growingSegmentEnd.y -= 1
+      this.growingSegmentLength += 1
+    } 
+    // Create a new branch segment
+    else {
+      this.growingSegmentEnd = this.addPoint(this.growingSegmentEnd.x, this.growingSegmentEnd.y)
+      this.growingSegmentLength = 0
+      this.growingSegmentMaxLength = this.randomSegmentLength()
+    }
   }
 
   render() {
+    if (this.points.length < 10) this.grow()
+
     this.builder.moveTo(this.points[0].x, this.points[0].y);
 
     this.points.forEach((point, index) => {
       this.builder.lineStyle({
-        width: this.points.length - index,
+        width: this.points.length - index + 1,
         color: COLOR_PINK,
         join: 'round',
         cap: 'round'
