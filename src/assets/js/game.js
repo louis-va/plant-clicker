6043cb45659d7e3f679e3f85d258fed3cc3d1d5e
Louis Van Aken <vanakenlouis@gmail.com>
2023-10-26 00:11:03 +0200

    Improved syntax and comments

---
+++ src/assets/js/game.js
@@ -0,0 +1,159 @@
+/*
+Author : SIVIXAY Celestin (Yaeshin)
+*/
+
+// Retrieve user data from localStorage or create a new user if not exists
+let currentUser = JSON.parse(localStorage.getItem("user"));
+if (currentUser == null) {
+    let user = {
+        score: 0,
+        bank: 0,
+        multiplier: 1,
+        autoclick: 0,
+        boost: false
+    };
+    localStorage.setItem("user", JSON.stringify(user));
+    currentUser = user;
+}
+
+// Update HTML elements with initial user data
+updateUI();
+
+// Event listener for clicking on the plant
+document.getElementById("plantClicker").addEventListener("click", () => {
+    currentUser = setScore(currentUser, getMultiplier());
+    currentUser = setBank(currentUser, getMultiplier());
+    updateUI();
+});
+
+// Event listener for purchasing autoclicker
+document.getElementById("purchaseAutoClick").addEventListener("click", () => {
+    handlePurchase(getAutoClickerPrice(), "autoclick");
+});
+
+// Event listener for purchasing multiplier
+document.getElementById("purchaseMultiplier").addEventListener("click", () => {
+    handlePurchase(getMultiplierPrice(), "multiplier");
+});
+
+// Event listener for purchasing boost
+document.getElementById("purchaseBoost").addEventListener("click", () => {
+    handlePurchase(getBoostPrice(currentUser.multiplier), "boost");
+});
+
+// Handle purchases
+function handlePurchase(price, type) {
+    if (currentUser.bank < price) {
+        alert("You don't have enough points!");
+    } else {
+        currentUser = setBank(currentUser, -price);
+        switch (type) {
+            case "autoclick":
+                currentUser = increaseAutoClicker(currentUser);
+                break;
+            case "multiplier":
+                currentUser = increaseMultiplier(currentUser);
+                break;
+            case "boost":
+                currentUser = boostON(currentUser);
+                setTimeout(boostOFF, 30000);
+                break;
+        }
+        updateUI();
+    }
+}
+
+// Update the UI with current user data
+function updateUI() {
+    document.getElementById("bank").innerHTML = currentUser.bank;
+    document.getElementById("amountAutoClick").innerHTML = currentUser.autoclick;
+    document.getElementById("autoClickPrice").innerHTML = getAutoClickerPrice();
+    document.getElementById("amountMultiplier").innerHTML = currentUser.multiplier;
+    document.getElementById("multiplierPrice").innerHTML = getMultiplierPrice();
+    document.getElementById("boostPrice").innerHTML = getBoostPrice(currentUser.multiplier);
+}
+
+// Booster
+function boostON(user){
+    user.boost=true;
+    localStorage.setItem("user",JSON.stringify(user));
+    return user;
+}
+
+function boostOFF() {
+    currentUser.boost = false;
+    localStorage.setItem("user", JSON.stringify(currentUser));
+    updateUI();
+}
+
+// Autoclicker
+setInterval(autoClick, 1000);
+
+function autoClick() {
+    currentUser = setScore(currentUser, getMultiplier() * getAutoClicker());
+    currentUser = setBank(currentUser, getMultiplier() * getAutoClicker());
+    updateUI();
+}
+
+// Incrementors
+function increaseAutoClicker(user) {
+    user.autoclick++;
+    localStorage.setItem("user", JSON.stringify(user));
+    return user;
+}
+
+function increaseMultiplier(user) {
+    user.multiplier++;
+    localStorage.setItem("user", JSON.stringify(user));
+    return user;
+}
+
+// Setters
+function setScore(user, value){
+    if(user.boost){
+        value=value*3;
+    }
+    user.score=user.score+value;
+    localStorage.setItem("user",JSON.stringify(user));
+    return user;
+}
+
+function setBank(user, value){
+    if(user.boost && value>0){
+        value=value*3;
+    }
+    user.bank=user.bank+value;
+    localStorage.setItem("user",JSON.stringify(user));
+    return user;
+}
+
+// Getters
+function getScore() {
+    return JSON.parse(localStorage.getItem("user")).score;
+}
+
+function getBank() {
+    return JSON.parse(localStorage.getItem("user")).bank;
+}
+
+function getMultiplier() {
+    return JSON.parse(localStorage.getItem("user")).multiplier;
+}
+  
+function getMultiplierPrice() {
+    let multiplier = JSON.parse(localStorage.getItem("user")).multiplier;
+    return 50 * Math.pow(2, multiplier);
+}
+
+function getAutoClicker(){
+    return JSON.parse(localStorage.getItem("user")).autoclick;
+}
+
+function getAutoClickerPrice() {
+  let autoclick = JSON.parse(localStorage.getItem("user")).autoclick;
+  return Math.round(100 * Math.pow(1.5, autoclick));
+}
+
+function getBoostPrice(multiplier){
+    return Math.round(200*multiplier);
+}
--- src/main.js
+++ src/main.js
@@ -2,5 +2,4 @@ import "./style.css";
 
 import './assets/js/plant/plant'
 import "./assets/js/topBar";
-import "./assets/js/panel";
-import './assets/js/backend';
+import './assets/js/game';
