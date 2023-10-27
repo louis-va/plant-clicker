6a1cd7237647ab0404081f2916e8a217a0915286
matthieuGravy <matthieugravy@gmail.com>
2023-10-27 10:52:17 +0200

    modal sans style

--- src/assets/js/topBar.js
+++ src/assets/js/topBar.js
@@ -1,21 +1,32 @@
 const articleModal = document.getElementById("article-modal");
 const buttonRules = document.getElementById("button-rules");
-const croix = document.getElementById("croix");
+const buttonStats = document.getElementById("button-stats");
+const croixRules = document.getElementById("croix-rules");
+const croixStats = document.getElementById("croix-stats");
 const cache = document.getElementById("cache");
+const stat = document.getElementById("stat");
 
-function togle() {
-  if (
-    articleModal.style.display === "none" ||
-    articleModal.style.display === ""
-  ) {
-    articleModal.style.display = "block";
+function togle(el) {
+  if (el.style.display === "none" || el.style.display === "") {
+    el.style.display = "block";
     cache.style.display = "block";
   } else {
-    articleModal.style.display = "none";
+    el.style.display = "none";
     cache.style.display = "none";
   }
 }
 
-buttonRules.addEventListener("click", togle);
+buttonRules.addEventListener("click", () => {
+  togle(articleModal);
+});
 
-croix.addEventListener("click", togle);
+croixRules.addEventListener("click", () => {
+  togle(articleModal);
+});
+
+buttonStats.addEventListener("click", () => {
+  togle(stat);
+});
+croixStats.addEventListener("click", () => {
+  togle(stat);
+});
